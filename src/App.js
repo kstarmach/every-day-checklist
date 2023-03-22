1cc5b52d8aead8594ce85106413f2cdfbbfb50f8
kstarmach <kamil.starmach@hotmail.com>
2023-03-22 19:26:34 +0100

    Initial commit: Added basic project structure and setup files.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Container } from '@mui/material'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div>
+      <Container>
+        <Header title="Every Day Checklist" />
+      </Container>
+    </div>
+  )
+}
+
+export default App
