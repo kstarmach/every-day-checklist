77e86e6091c08ccf6024706fc10345604e725098
Kamil Starmach <kstarmach@outlook.com>
2023-12-06 15:58:52 +0100

    fix: Odd error

--- server/models/todo.ts
+++ server/models/todo.ts
@@ -1,37 +1,42 @@
 import mongoose, { Model, Schema } from 'mongoose';
-import { TodoDocument, Todo } from '../utils/types';
+import { Todo } from '../utils/types';
 
-const todoSchema = new Schema<Todo>({
-    text: {
-        type: String,
-        required: true
+const todoSchema = new Schema<Todo>(
+    {
+        text: {
+            type: String,
+            required: true
+        },
+        done: {
+            type: Boolean,
+            default: false,
+            required: true
+        },
+        createDate: {
+            type: Date,
+            required: true
+        },
+        order: {
+            type: Number,
+            required: false
+        },
+        updateDate: {
+            type: Date,
+            required: false
+        }
     },
-    done: {
-        type: Boolean,
-        default: false,
-        required: true
-    },
-    createDate: {
-        type: Date,
-        required: true
-    },
-    order: {
-        type: Number,
-        required: false
-    },
-    updateDate: {
-        type: Date,
-        required: false
+    {
+        toJSON: {
+            transform: function (_, ret) {
+                // Remove _id and __v, and replace _id with id
+                ret.id = ret._id;
+                delete ret._id;
+                delete ret.__v;
+            }
+        }
     }
-});
+);
 
-todoSchema.set('toJSON', {
-    transform: (_document: unknown, returnedObject: TodoDocument) => {
-        returnedObject.id = returnedObject._id ? returnedObject._id.toString() : '';
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    }
-});
 
 const TodoModel: Model<Todo> = mongoose.model<Todo>('Todo', todoSchema);
 
