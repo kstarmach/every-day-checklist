26e064c4551be073e47a8b9b06cbe9f60d5a749a
Kamil Starmach <kstarmach@outlook.com>
2023-12-13 23:36:12 +0100

    feat: Mock user data

--- client/src/components/Sidebar/UserInfo.tsx
+++ client/src/components/Sidebar/UserInfo.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { red } from '@mui/material/colors';
 import AccountMenu from './AccountMenu';
 
@@ -7,7 +7,19 @@ import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
+
+
+interface userData {
+    id: number,
+    name: {
+        firstName: string,
+        lastName: string
+    },
+    email: string
+}
+
 const UserInfo = () => {
+    const [userData, setUserData] = useState<null | userData>(null);
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
     const open = Boolean(anchorEl);
     const handleClick = (event: React.MouseEvent<HTMLElement>) => {
@@ -16,30 +28,50 @@ const UserInfo = () => {
     const handleClose = () => {
         setAnchorEl(null);
     };
-    return (
-        <>
-            <CardHeader
-                avatar={
-                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                        KS
-                    </Avatar>
-                }
-                action={
-                    <IconButton aria-label="settings" onClick={handleClick}>
-                        <MoreVertIcon />
-                    </IconButton>
-                }
-                title="Kamil Starmach"
-                subheader="starmil96@gmail.com"
-            />
-            <AccountMenu
-                handleClose={handleClose}
-                anchorEl={anchorEl}
-                open={open}
-            />
 
-        </>
-    )
+
+    useEffect(() => {
+        const newUserData = {
+            id: 12345,
+            name: {
+                firstName: "Kamil",
+                lastName: "Starmach",
+            },
+            email: "starmil96@gmail.com",
+        }
+
+        setUserData(newUserData);
+    }, [])
+
+
+    if (userData) {
+
+        return (
+            <>
+                <CardHeader
+                    avatar={
+                        <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                            KS
+                        </Avatar>
+                    }
+                    action={
+                        <IconButton aria-label="settings" onClick={handleClick}>
+                            <MoreVertIcon />
+                        </IconButton>
+                    }
+                    title={userData.name.firstName + ' ' + userData.name.lastName}
+                    subheader={userData.email}
+                />
+                <AccountMenu
+                    handleClose={handleClose}
+                    anchorEl={anchorEl}
+                    open={open}
+                />
+
+            </>
+        )
+    }
+    return '';
 }
 
 export default UserInfo
